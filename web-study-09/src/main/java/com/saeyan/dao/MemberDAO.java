8c6058906ae38745000a10b50295c0a5d3d31cf1
AhnChanghyun <ach880203@gmail.com>
2025-11-13 14:56:37 +0900

    JSP Study

--- web-study-09/src/main/java/com/saeyan/dao/MemberDAO.java
+++ web-study-09/src/main/java/com/saeyan/dao/MemberDAO.java
@@ -2,6 +2,8 @@ package com.saeyan.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /*
  * DB연결을 담당하는 클래스
@@ -10,37 +12,72 @@ import java.sql.DriverManager;
  */
 public class MemberDAO {
 
-	//1. 싱글톤 패턴 : 객체를 하나만 생성
-	private static MemberDAO instance  = new MemberDAO();
-	
-	//2. 외부에서 생성자 호출 못하도록 Private
-	 private MemberDAO() {
-		 
-	 }
-	 
-	 //3. 외부에서 사용할 수 있도록 간일 인스턴스 제공
-	 public static MemberDAO getInstance() {
-		 return instance;
-	 }
-	 
-	 //4. DB 연결 메소드(JDBC)
-	 public Connection getConnection() {
-		 
-		 Connection con = null;
-		 
-		 try {
-			 
-			 Class.forName("oracle.jdbc.driver.OracleDriver");
-			 
-			 con = DriverManager.getConnection(
-					 "jdbc:oracle:thin:@localhost:1521:xe",
-					 "test",
-					 "1234");
-			 
-		       //드라이브 로드	 
-		 }catch(Exception e){
-		    	e.printStackTrace();
-		 }
-		  return con;
-	 }
+	// 1. 싱글톤 패턴 : 객체를 하나만 생성
+	private static MemberDAO instance = new MemberDAO();
+
+	// 2. 외부에서 생성자 호출 못하도록 Private
+	private MemberDAO() {
+
+	}
+
+	// 3. 외부에서 사용할 수 있도록 간일 인스턴스 제공
+	public static MemberDAO getInstance() {
+		return instance;
+	}
+
+	// 4. DB 연결 메소드(JDBC)
+	public Connection getConnection() {
+
+		Connection con = null;
+
+		try {
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "test", "1234");
+
+			// 드라이브 로드
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	// 사용자 인증시 사용되는 메소드
+	public int userCheck(String userid, String pwd) {
+		int result = -1;
+
+		String sql = "slect pwd from member where userid = ?";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			con = getConnection();//DB연결
+			pstmt = con.prepareStatement(sql);//sql 구문 전송.. sql 에러있니 없니 체크 
+			pstmt.setString(1, userid);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				//가져올 데이터 있니?
+			}
+				
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				pstmt.close();
+				con.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return 0;
+	}
 }
