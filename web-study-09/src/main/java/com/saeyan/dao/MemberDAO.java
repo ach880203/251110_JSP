e0aa046bf9b4cd411fc7bbbb594c8e4f0d2efaaf
AhnChanghyun <ach880203@gmail.com>
2025-11-14 18:51:24 +0900

    JSP Study

--- web-study-09/src/main/java/com/saeyan/dao/MemberDAO.java
+++ web-study-09/src/main/java/com/saeyan/dao/MemberDAO.java
@@ -200,4 +200,34 @@ public class MemberDAO {
 		return result;
 	}// end insertMember
 
-}
+	public void updateMember(MemberVO mvo) {
+		
+		String sql = "update member set pwd=?, email=?, phone=?, admin=? where userid=?";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = this.getConnection();// DB연결
+			pstmt = con.prepareStatement(sql);// sql 구문 전송.. sql 에러있니 없니 체크
+			
+			pstmt.setString(1, mvo.getPwd());
+			pstmt.setString(2, mvo.getEmail());
+			pstmt.setString(3, mvo.getPhone());
+			pstmt.setInt(4, mvo.getAdmin());
+			pstmt.setString(5, mvo.getUserid());
+
+			pstmt.executeUpdate(); // 가져올 데이터 있음?
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				con.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} // end finally
+	}}
+
+
