c399513e8ff19fe7170acdd1338f374892485933
AhnChanghyun <ach880203@gmail.com>
2025-11-17 19:00:28 +0900

    JSP Study

--- web-study-10/src/main/java/com/saeyan/controller/ProductWriteServlet.java
+++ web-study-10/src/main/java/com/saeyan/controller/ProductWriteServlet.java
@@ -1,11 +1,16 @@
 package com.saeyan.controller;
 
 import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 
 @WebServlet("/productWrite.do")
@@ -23,6 +28,28 @@ public class ProductWriteServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 
+		request.setCharacterEncoding("utf-8");
+		
+		ServletContext context = getServletContext();
+		System.out.println("context : " + context);
+		
+		String path = context.getRealPath("upload");
+		System.out.println("path : " + path);
+		
+		String encType = "utf-8";
+		
+		int sizeLimit = 20*1024*1024;//파일용량 크기 20MB 제한
+	
+		MultipartRequest multi = 
+				new MultipartRequest(request, encType, sizeLimit, new DefaultFileRenamePolicy());
+		
+	
+	String name = multi.getParameter("name");
+	int price = Integer.parseInt(multi.getParameter("price"));
+	String pictureUrl = multi.getFilesystemName("pictureUrl");
+	String description = multi.getParameter("description");
+	
 	}
 
 }
